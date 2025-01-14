627318956a817c94a7b32962a61386f923a2889e
Carlos Henrique <ichfs9108@gmail.com>
2025-01-13 22:55:43 -0300

    feat: criando as entidades do jbank (wallet, deposit e transfer)

---
+++ jbank/src/main/java/tech/buildrun/jbank/entities/Deposit.java
@@ -0,0 +1,80 @@
+package tech.buildrun.jbank.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_deposits")
+public class Deposit {
+
+    @Id
+    @Column(name = "deposit_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID depositId;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @Column(name = "deposit_value")
+    private BigDecimal depositValue;
+
+    @Column(name = "deposit_date_time")
+    private LocalDateTime depositDateTime;
+
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    public Deposit() {
+    }
+
+    public UUID getDepositId() {
+        return depositId;
+    }
+
+    public void setDepositId(UUID depositId) {
+        this.depositId = depositId;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public BigDecimal getDepositValue() {
+        return depositValue;
+    }
+
+    public void setDepositValue(BigDecimal depositValue) {
+        this.depositValue = depositValue;
+    }
+
+    public LocalDateTime getDepositDateTime() {
+        return depositDateTime;
+    }
+
+    public void setDepositDateTime(LocalDateTime depositDateTime) {
+        this.depositDateTime = depositDateTime;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
